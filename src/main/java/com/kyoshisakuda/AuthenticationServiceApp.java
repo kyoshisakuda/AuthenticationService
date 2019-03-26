a4a4e84e3bd669ae48c3b32fbd2ada544d7659e7
Kyoshi Sakuda <kyoshi.sakuda@gmail.com>
2019-03-26 15:48:39 -0500

    Main class created

---
+++ src/main/java/com/kyoshisakuda/AuthenticationServiceApp.java
@@ -0,0 +1,13 @@
+package com.kyoshisakuda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationServiceApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthenticationServiceApp.class, args);
+    }
+
+}
