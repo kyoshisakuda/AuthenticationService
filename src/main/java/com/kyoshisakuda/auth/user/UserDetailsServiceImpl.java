c1ec4b02f650dcb8e0e2c07c640a2a38926dbd77
Kyoshi Sakuda <kyoshi.sakuda@gmail.com>
2019-03-26 16:28:45 -0500

    User loader added

---
+++ src/main/java/com/kyoshisakuda/auth/user/UserDetailsServiceImpl.java
@@ -0,0 +1,24 @@
+package com.kyoshisakuda.auth.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.List;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private PasswordEncoder encoder;
+
+    @Autowired
+    private UserService service;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return service.getUser(username).orElseThrow(() -> new UsernameNotFoundException(username));
+    }
+
+}
